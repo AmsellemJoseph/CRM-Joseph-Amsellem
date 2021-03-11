083e1e7dbb207a8ec5cd75349ba6f258aaba89c1
Joseph Amsellem <amsellem.joseph@gmail.com>
2021-03-11 17:34:17 +0200

    maj 11/03 17h33

--- js/components/login/view.js
+++ js/components/login/view.js
@@ -15,11 +15,17 @@ function login() {
 }
 
 function init() {
-    main.innerHTML = `<div class="containerUserName">
+    main.innerHTML = `<div id="containerBase">
+    <div>
+    <div class="style1"></div>
+    <div class="style2"></div>
+    <div class="style3"></div>
+    </div>
+    <div class="containerUserName">
     <h1>Please enter your username</h1>
     <input type="text" id="userName" placeholder="username">
 <button id="btnUsername">Enter</button>
-    </div>`
+    </div></div>`
 }
 
 function champion() {
